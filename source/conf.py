16b8357e213b92beb7b20f2d3f50b169d37c9287
Kenji SATO <kenji@kenjisato.jp>
2015-06-24 10:38:49 +0900

    Use MathJax

--- source/conf.py
+++ source/conf.py
@@ -38,7 +38,7 @@ extensions = [
     'sphinx.ext.mathjax',
     'sphinx.ext.ifconfig',
     'sphinx.ext.viewcode',
-    'matplotlib.sphinxext.mathmpl',
+    #'matplotlib.sphinxext.mathmpl',
     'matplotlib.sphinxext.only_directives',
     'matplotlib.sphinxext.plot_directive',
     'IPython.sphinxext.ipython_console_highlighting',
